18b01c3f51fd0047f4ed269fb5f06d6887543711
Mark Lilback <mark@lilback.com>
2019-09-08 17:09:10 -0400

    updated for swift 5

--- Package.swift
+++ Package.swift
@@ -1,3 +1,8 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 /*
  * Package.swift
  * Copyright (c) 2015 Ben Gollmer.
@@ -13,11 +18,31 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * modified by Mark Lilback for swift 5
  */
 
-import PackageDescription
-
 let package = Package(
-  name: "CommandLine",
-  exclude: ["script"]
+    name: "CommandLine",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CommandLine",
+            targets: ["CommandLine"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CommandLine",
+            dependencies: [],
+            exclude: ["scripts"]),
+        .testTarget(
+            name: "CommandLineTests",
+            dependencies: ["CommandLine"]),
+    ]
 )
